c5a413688386fa00007294a9c13a407c4310e40b
nicolassutter <contact.sutter.nicolas@gmail.com>
2023-04-29 19:25:25 +0200

    feat: add daisyui and mdi icon pack

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,9 @@
 import type { Config } from 'tailwindcss'
+import daisyUi from 'daisyui'
 // import defaultTheme from 'tailwindcss/defaultTheme'
 
 export default <Partial<Config>>{
+  plugins: [daisyUi],
   theme: {
     extend: {},
   },
