86c53d88dd6bd436ed73df54d1b6efa26c2b0390
nicolassutter <contact.sutter.nicolas@gmail.com>
2023-04-29 10:38:24 +0200

    chore: install unplugin-icons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,17 @@
+import type { Options as UnpluginIconsOptions } from 'unplugin-icons/types'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: [
     '@sidebase/nuxt-auth',
     '@nuxtjs/tailwindcss',
     '@huntersofbook/naive-ui-nuxt',
+    [
+      'unplugin-icons/nuxt',
+      <UnpluginIconsOptions>{
+        /* options */
+      },
+    ],
   ],
   build: {
     transpile: ['trpc-nuxt'],
