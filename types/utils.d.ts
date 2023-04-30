388f11a40badd46488021b4131f0fa932934b0a4
nicolassutter <contact.sutter.nicolas@gmail.com>
2023-04-30 20:52:08 +0200

    chore: add utility types to compute a specific component type

--- types/utils.d.ts
+++ types/utils.d.ts
@@ -1,4 +1,9 @@
+import type { GlobalComponents } from 'vue'
+
 export type PathOption = {
   path: string
   type: 'directory' | 'file'
 }
+
+export type ComponentType<K extends keyof GlobalComponents> = GlobalComponents[K] extends new (...args: any[]) => infer R ? R : never
+export type ComponentProps<K extends keyof GlobalComponents> = ComponentType<K>['$props']
